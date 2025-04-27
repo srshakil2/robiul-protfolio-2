cad3d16e5d94bca0da186b1d400a7153962434ab
srshakil2 <rh3642921@gmail.com>
2025-04-27 14:03:23 +0600

    collect this forms data

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,28 +1,37 @@
 import SectionTitle from "../../Hooks/SectionTitle/SectionTitle";
 import Container from "../../Hooks/Container";
-
-import { useForm, ValidationError } from "@formspree/react";
 import Swal from "sweetalert2";
 import LottieAnimation from "../LottieAnimation/LottieAnimation";
 
 const ContactForm = () => {
-  const [state, handleSubmit, resetForm] = useForm("xoqograg");
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    const formsData = e.target;
+    const name = formsData.name.value;
+    const email = formsData.email.value;
+    const massage = formsData.message.value;
 
-  if (state.succeeded && !state.submitting) {
-    Swal.fire({
-      title: "Thanks for your feedback!",
-      text: "I will contact you in a little while, give me some time.",
-      icon: "success",
-    });
-    // Wait for the resetForm Promise to resolve
-    const form = document.getElementById("yourFormId");
+    const data = {
+      name: name,
+      email: email,
+      massage: massage,
+    };
+    // Todo: send this data in server is ready
+    console.log(data);
+  };
+  // if (state.succeeded && !state.submitting) {
+  //   Swal.fire({
+  //     title: "Thanks for your feedback!",
+  //     text: "I will contact you in a little while, give me some time.",
+  //     icon: "success",
+  //   });
+  //   // Wait for the resetForm Promise to resolve
+  //   const form = document.getElementById("yourFormId");
 
-    if (form) {
-      form.reset();
-    }
-
-    resetForm({ submitting: true });
-  }
+  //   if (form) {
+  //     form.reset();
+  //   }
+  // }
 
   return (
     <Container>
@@ -30,67 +39,40 @@ const ContactForm = () => {
         <SectionTitle title={"Contact Me"}></SectionTitle>
         <div className="flex  flex-col-reverse md:flex-row-reverse  justify-between  text-[#F2F2F2] items-center gap-10 ">
           <div className="w-full md:w-1/2">
-            <form
-              id="yourFormId"
-              onSubmit={(e) => {
-                handleSubmit(e);
-              }}
-            >
+            <form onSubmit={(e) => handelSubmit(e)}>
               <div className="flex gap-5 justify-between items-center h-auto">
                 <div className="w-1/2">
                   <input
                     type="text"
-                    id="name"
                     name="name"
                     placeholder="Name"
                     required
                     className="mt-1 p-2 bg-transparent border-b rounded-md w-full"
                   />
-                  <ValidationError
-                    prefix="Name"
-                    field="name"
-                    errors={state.errors}
-                  />
                 </div>
                 <div className="w-1/2">
                   <input
                     type="email"
-                    id="email"
                     name="email"
                     placeholder="Email"
                     required
                     className="mt-1 p-2 border-b bg-transparent rounded-md w-full"
-                  />
-                  <ValidationError
-                    prefix="Email"
-                    field="email"
-                    errors={state.errors}
                   />
                 </div>
               </div>
 
               <div className="mt-10">
                 <textarea
-                  id="message"
                   name="message"
                   placeholder="Message"
                   required
                   className="mt-1 pt-5 pl-2 border-b bg-transparent rounded-md w-full"
                 ></textarea>
-                <ValidationError
-                  prefix="Message"
-                  field="message"
-                  errors={state.errors}
-                />
               </div>
-
               <button
                 type="submit"
                 className="btn hover:bg-transparent w-full mt-4 font-bold bg-transparent text-[#70FF00] flex items-center gap-3 border-2  px-3 rounded-md"
               >
-                {state?.submitting && (
-                  <span className="loading loading-spinner loading-md pl-2"></span>
-                )}
                 Send
               </button>
             </form>
